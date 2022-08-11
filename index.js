0da01c45f1fa0197964de1eaa3d1ee733d56fcd7
KawserAlam21 <mdnaiyem.islam.10@gmail.com>
2022-08-11 23:50:55 +0600

    added get product and post product route

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const bodyParser = require("body-parser");
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion } = require("mongodb");
 const cors = require("cors");
 require("dotenv").config();
 
@@ -11,28 +11,40 @@ const PORT = process.env.PORT;
 app.use(cors());
 app.use(bodyParser.json());
 
-
 //mongodb connection
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.6w64q.mongodb.net/?retryWrites=true&w=majority`;
-const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
-
+console.log(uri);
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverApi: ServerApiVersion.v1,
+});
 
 async function run() {
   try {
+    await client.connect();
     const database = client.db("FreshMart");
     const productCollection = database.collection("products");
 
+    //loading products form database
+    app.get("/products", async (req, res) => {
+      const product = await productCollection.find().toArray();
+      res.send(product);
+    });
 
+    //sending product information to database
+    app.post("/products", async (req, res) => {
+      const product = req.body;
+      const result = await productCollection.insertOne(product);
+      res.send(result);
+    });
 
-
-    console.log('FreshMart Database Connected Successfully');
+    console.log("FreshMart Database Connected Successfully");
   } finally {
-    await client.close();
+    // await client.close();
   }
 }
 run().catch(console.dir);
-
-
 
 app.get("/", (req, res) => {
   res.send("Freshmart Co-op Server Is Running");
