6e21eb3c60b7dd3727e60a4f064a45c6b0603576
KawserAlam21 <mdnaiyem.islam.10@gmail.com>
2022-08-12 17:48:22 +0600

    added single product route and delete route

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const { MongoClient, ServerApiVersion } = require("mongodb");
+const ObjectId = require("mongodb").ObjectId;
 const cors = require("cors");
 require("dotenv").config();
 
@@ -13,7 +14,6 @@ app.use(bodyParser.json());
 
 //mongodb connection
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.6w64q.mongodb.net/?retryWrites=true&w=majority`;
-console.log(uri);
 const client = new MongoClient(uri, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
@@ -31,12 +31,31 @@ async function run() {
       const product = await productCollection.find().toArray();
       res.send(product);
     });
+    // loading single product by id
+    app.get("/products/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: ObjectId(id) };
+      const product = await productCollection.findOne(query);
+      res.send(product);
+    });
 
     //sending product information to database
     app.post("/products", async (req, res) => {
       const product = req.body;
       const result = await productCollection.insertOne(product);
       res.send(result);
+    });
+
+    // removing data from Database
+    app.delete("/products/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: ObjectId(id) };
+      const result = await productCollection.deleteOne(query);
+      if (result.deletedCount === 1) {
+        res.send(result);
+      } else {
+        console.log("No documents matched the query. Deleted 0 documents.");
+      }
     });
 
     console.log("FreshMart Database Connected Successfully");
